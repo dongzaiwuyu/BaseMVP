1df45aac2d42caa5573ea3afe82df0fcd9428aa6
lxd <linxiaodong@ecaicn.com>
2020-07-30 14:04:24 +0800

    BaseFragment

--- app/src/main/java/com/don/basemvp/base/BaseFragment.java
+++ app/src/main/java/com/don/basemvp/base/BaseFragment.java
@@ -2,10 +2,14 @@ package com.don.basemvp.base;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.PopupWindow;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.don.basemvp.event.BaseEvent;
 import com.don.basemvp.presenter.IPresenter;
@@ -17,48 +21,60 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
-/***
- *@author Don
- *@date on 2020/4/23 13:50
- *@describe 基础Activity
- */
-public abstract class BaseActivity<V extends IBaseView, P extends IPresenter> extends AppCompatActivity implements IBaseView {
+public abstract class BaseFragment<V extends IBaseView, P extends IPresenter> extends Fragment implements IBaseView {
 
     protected P presenter;
     protected V view;
+    private Unbinder unbinder;
     protected BaseLoadingPopupWindow popupWindow;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getContentLayout());
-        ButterKnife.bind(this);
         presenter = createPresenter();
-        if(presenter!=null) {
+        if (presenter != null) {
             presenter.attachView(this);
         }
         EventBus.getDefault().register(this);
     }
 
+    @Nullable
     @Override
-    protected void onDestroy() {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getContentLayout(), container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+    }
+
+    @Override
+    public void onDestroyView() {
         EventBus.getDefault().unregister(this);
+        if (unbinder != null) {
+            unbinder.unbind();
+            unbinder = null;
+        }
         if (getLoadingPop().isShowing()) {
             getLoadingPop().dismiss();
         }
         if (presenter != null) {
             presenter.detachView();
         }
-        super.onDestroy();
+        super.onDestroyView();
     }
 
     @Override
     public void setPresenter(IPresenter presenter) {
+
     }
 
     public void showLoading() {
-        getLoadingPop().showAtLocation(this.getWindow().getDecorView(), Gravity.CENTER, 0, 0);
+        getLoadingPop().showAtLocation(this.getActivity().getWindow().getDecorView(), Gravity.CENTER, 0, 0);
     }
 
     public void dissLoading() {
@@ -67,18 +83,19 @@ public abstract class BaseActivity<V extends IBaseView, P extends IPresenter> ex
 
     public PopupWindow getLoadingPop() {
         if (popupWindow == null) {
-            popupWindow = new BaseLoadingPopupWindow(this);
+            popupWindow = new BaseLoadingPopupWindow(this.getContext());
             popupWindow.UpdateLoadingTips("加载中...");
         }
         return popupWindow;
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void EventUpdate(BaseEvent event)
-    {
+    public void EventUpdate(BaseEvent event) {
     }
 
     protected abstract P createPresenter();
 
     protected abstract int getContentLayout();
+
+    public abstract void init();
 }
